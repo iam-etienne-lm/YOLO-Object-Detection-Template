6cf44a072fbf65fcb4c1ce5f30aef56bdae0012d
dodalpaga <dorian.voydie@gmail.com>
2022-05-04 10:41:33 +0200

    Ajout de l'entrainement All Spaghetti

--- ressources/scripts/metrics.php
+++ ressources/scripts/metrics.php
@@ -1,5 +1,5 @@
 <?php
 header("Location: http://localhost:6006");
 $output = shell_exec("cd ../../YOLO && xdg-open http://localhost:6006 && nohup ../venv/bin/python3 -m tensorboard.main --logdir ./yolov5/runs/train &");
-// echo $output;
+echo $output;
 ?>
